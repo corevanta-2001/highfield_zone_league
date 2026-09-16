96c02bc54df888d6b620efbfc90537ce7844a57f
corevanta-2001 <corevantaprojects@gmail.com>
2026-09-16 17:24:45 +0000

    ready for play store build - v1.0.0+1

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,46 @@
+plugins {
+        id("com.android.application")
+            id("org.jetbrains.kotlin.android")
+                // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+                    id("dev.flutter.flutter-gradle-plugin")
+}
+
+android {
+        namespace =com.android.highfield_zone_league"
+            compileSdk = 36 
+                ndkVersion = flutter.ndkVersion
+
+                    compileOptions {
+                                sourceCompatibility = JavaVersion.VERSION_17
+                                        targetCompatibility = JavaVersion.VERSION_17
+                    }
+
+                        defaultConfig {
+                                    applicationId = "com.example.exam_hook"
+                                            minSdk = flutter.minSdkVersion
+                                                    targetSdk = flutter.targetSdkVersion
+                                                            versionCode = flutter.versionCode
+                                                                    versionName = flutter.versionName
+                        }
+
+                            buildTypes {
+                                        release {
+                                                        signingConfig = signingConfigs.getByName("debug")
+                                                                    isMinifyEnabled = false  // <-- ADD THIS
+                                                                                isShrinkResources = false // <-- ADD THIS
+                                        }
+                            }
+}
+
+kotlin {
+        compilerOptions {
+                    jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+        }
+}
+
+flutter {
+        source = "../.."
+}
+                                        }
+                    }
+}
