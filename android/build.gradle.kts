9e2a27c09111eaa8af2b5479b5844f973158b3c5
corevanta-2001 <corevantaprojects@gmail.com>
2026-09-16 20:18:17 +0000

    fix AGP 9 API - use LibraryExtension.compileSdk

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -18,13 +18,12 @@ tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
 
-// This runs AFTER all projects are evaluated - fixes the afterEvaluate bug
 gradle.projectsEvaluated {
     subprojects {
-        val androidExt = extensions.findByName("android")
-        if (androidExt is com.android.build.gradle.BaseExtension) {
-            if (androidExt.compileSdkVersion < 36) {
-                androidExt.compileSdkVersion(36)
+        plugins.withId("com.android.library") {
+            val libExt = extensions.findByName("android") as? com.android.build.api.dsl.LibraryExtension
+            if (libExt != null && libExt.compileSdk < 36) {
+                libExt.compileSdk = 36
                 println("Forced ${project.name} compileSdk to 36")
             }
         }
