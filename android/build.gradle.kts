7613d77883ad0bd895f9a820ace2d8474207ae9b
corevanta-2001 <corevantaprojects@gmail.com>
2026-09-16 20:11:21 +0000

    force all subprojects to compileSdk 36 in projectsEvaluated

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -6,19 +6,27 @@ allprojects {
 }
 
 val newBuildDir: Directory =
-    rootProject.layout.buildDirectory
-        .dir("../../build")
-        .get()
+    rootProject.layout.buildDirectory.dir("../../build").get()
 rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
-subprojects {
-    project.evaluationDependsOn(":app")
-}
 
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
+}
+
+// This runs AFTER all projects are evaluated - fixes the afterEvaluate bug
+gradle.projectsEvaluated {
+    subprojects {
+        val androidExt = extensions.findByName("android")
+        if (androidExt is com.android.build.gradle.BaseExtension) {
+            if (androidExt.compileSdkVersion < 36) {
+                androidExt.compileSdkVersion(36)
+                println("Forced ${project.name} compileSdk to 36")
+            }
+        }
+    }
 }
